6ad56effcc3ae08173cd9a1baa935dbf3516403e
inahsi-k <ishanikarpoor02@gmail.com>
2025-12-15 16:04:51 +0530

    added login and signup for students

--- server/models/user.js
+++ server/models/user.js
@@ -1,9 +1,26 @@
 const mongoose= require ('mongoose')
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  password: { type: String, required: true }, 
-  email:{type:String,required:true},
+  name: { 
+    type: String, 
+    required: true 
+  },
+  password: { 
+    type: String, 
+    required: true 
+  }, 
+  email:{
+    type:String,
+    required:true
+  },
+  batch: {
+    startYear: { type: Number, required: true, min: 2000, max: 2100 },
+    endYear:   { type: Number, required: true, min: 2000, max: 2100 }
+  },
+  rollNo:{
+    type:Number,
+    unique:true,
+  },
   role: { 
     type: String, 
     default: 'volunteer' 
