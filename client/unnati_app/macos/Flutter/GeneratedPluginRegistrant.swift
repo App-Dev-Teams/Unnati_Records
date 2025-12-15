5de25bdfefe3470a5e3239690debe5054c00de09
urastogi2048 <utkarshrastogi2048@gmail.com>
2025-12-15 18:08:46 +0530

    Regenerate lockfile and plugins after merge

--- client/unnati_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ client/unnati_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,7 +11,6 @@ import pdfx
 import shared_preferences_foundation
 import syncfusion_pdfviewer_macos
 import url_launcher_macos
-import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
@@ -19,6 +18,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PdfxPlugin.register(with: registry.registrar(forPlugin: "PdfxPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SyncfusionFlutterPdfViewerPlugin.register(with: registry.registrar(forPlugin: "SyncfusionFlutterPdfViewerPlugin"))
-  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
