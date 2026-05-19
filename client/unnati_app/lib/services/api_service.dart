5b6f91aa795ca4ec3f15bec643d1a0510b778048
ItxPriyanshu <priyanshukumar3644@gmail.com>
2026-05-19 14:49:28 +0530

    minor bugs fixes

--- client/unnati_app/lib/services/api_service.dart
+++ client/unnati_app/lib/services/api_service.dart
@@ -3,8 +3,8 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiService {
-  static const String baseUrl = 'http://unnati.onrender.com/api/auth';
-  static const String coreBaseUrl = 'http://unnati.onrender.com/api';
+  static const String baseUrl = 'https://unnati-records.onrender.com/api/auth';
+  static const String coreBaseUrl = 'https://unnati-records.onrender.com/api';
   static const Duration _timeout = Duration(seconds: 30);
   static const Map<String, String> _headers = {
     'Content-Type': 'application/json',
