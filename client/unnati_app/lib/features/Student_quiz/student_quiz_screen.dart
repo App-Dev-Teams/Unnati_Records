d76600bdf3851f5018d67dc04ad956e19a95f859
ItxPriyanshu <priyanshukumar3644@gmail.com>
2026-06-01 18:52:22 +0530

    bottom nav bar fixed

--- client/unnati_app/lib/features/Student_quiz/student_quiz_screen.dart
+++ client/unnati_app/lib/features/Student_quiz/student_quiz_screen.dart
@@ -8,7 +8,7 @@ class StudentQuizScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      // backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
       ),
