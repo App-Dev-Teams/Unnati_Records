e864327f0f7127f79085f00b7ca2ba0e513cc2c5
urastogi2048 <utkarshrastogi2048@gmail.com>
2026-08-28 19:49:09 +0530

    feat:changes for production

--- client/unnati_app/android/app/build.gradle.kts
+++ client/unnati_app/android/app/build.gradle.kts
@@ -1,12 +1,30 @@
+import java.util.Properties
+import java.io.FileInputStream
+import org.gradle.api.GradleException
+
+val keystorePropertiesFile = rootProject.file("key.properties")
+val keystoreProperties = Properties()
+
+// 1. FORCE CRASH IF FILE IS MISSING
+if (!keystorePropertiesFile.exists()) {
+    throw GradleException("FATAL ERROR: key.properties file is missing at ${keystorePropertiesFile.absolutePath}")
+}
+
+keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+
+// 2. FORCE CRASH IF PASSWORDS ARE MISSING
+if (keystoreProperties["storePassword"] == null) {
+    throw GradleException("FATAL ERROR: Could not read passwords from key.properties!")
+}
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace = "com.example.unnati_app"
+    namespace = "com.unnati.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -20,21 +38,31 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.unnati_app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        applicationId = "com.unnati.app"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 }
